9aff22ecc90941d573749e6b0d0cb800fdfb7c46
shveni21 <shvenireddy@gmail.com>
2025-09-09 04:41:37 +0530

    Pushed Project Files

--- init/index.js
+++ init/index.js
@@ -1,59 +1,27 @@
-// const mongoose = require("mongoose");
-// const initData = require("./data.js");
-// const Listing = require("../models/listing.js");
-
-// const dbUrl = process.env.ATLASDB_URL;
-
-// main()
-//   .then(() => {
-//     console.log("connected to db");
-//   })
-//   .catch((err) => console.log(err));
-
-// async function main() {
-//   await mongoose.connect(dbUrl);
-// }
-
-// const initDB = async () => {
-//   await Listing.deleteMany({});
-//   initData.data = initData.data.map((obj) => ({
-//     ...obj,
-//     owner: "68bb325402e6f6eb74e5f870",
-//   }));
-//   await Listing.insertMany(initData.data);
-//   console.log("data was initialized");
-// };
-
-// initDB();
-
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
 const dbUrl = process.env.ATLASDB_URL;
 
+main()
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => console.log(err));
+
 async function main() {
-  try {
-    await mongoose.connect(dbUrl);
-    console.log("Connected to Atlas DB");
-
-    // Clear old data
-    await Listing.deleteMany({});
-
-    // Prepare new data
-    const dataToInsert = initData.data.map((obj) => ({
-      ...obj,
-      owner: "68bb325402e6f6eb74e5f870",
-    }));
-
-    // Insert data
-    await Listing.insertMany(dataToInsert);
-    console.log("Data was initialized");
-
-    mongoose.connection.close(); // close connection after seeding
-  } catch (err) {
-    console.error("Error connecting or seeding DB:", err);
-  }
+  await mongoose.connect(dbUrl);
 }
 
-main();
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "68bb325402e6f6eb74e5f870",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
